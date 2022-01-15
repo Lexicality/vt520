e7f0b4b0a281ea3e4fe56f255cfbafd850fa20f7
Lexi Robinson <lexi@lexi.org.uk>
2022-01-15 18:41:12 +0000

    Make the dvorak script actually work!
    Probably going to need an un-dvorak script at some point but that's
    really not my problem right now ;)

--- dvorak.py
+++ dvorak.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import os
+import select
+import sys
+import time
+import tty
 from typing import Dict
 
 KEYMAP: Dict[int, int] = {
@@ -55,11 +60,62 @@ KEYMAP: Dict[int, int] = {
     53: 18,
     54: 49,
     55: 46,
-    # Special
-    # Caps/Esc swap
-    110: 30,
-    30: 110,
 }
 
-switcheroo = ";".join(f"{src}/{dest}" for dest, src in KEYMAP.items())
-print('\N{DCS}"z' + switcheroo + "\N{ST}")
+# Chaos time!
+STDIN = sys.stdin.fileno()
+tty.setcbreak(STDIN)
+polly = select.poll()
+polly.register(STDIN, select.POLLIN | select.POLLERR)
+
+
+def readr() -> str:
+    # Blocking read on one byte because there should always be at least one
+    ret = os.read(STDIN, 1)
+    # Read everything else that's available
+    # (I'm sure the terminal will write fast enough)
+    while polly.poll(0.1):
+        ret += os.read(STDIN, 20)
+    return ret.decode("latin-1")
+
+
+def get_keydef(key: int) -> str:
+    print(f"\N{CSI}{key},w", end=" ", flush=True)  # DECRQKD 5-106
+    decrpak = readr()
+    # These are glorious magical print statments that make the damn thing
+    #  actually work, I'm assuming it's something to do with flow control or
+    #  something
+    print(key, repr(decrpak))
+    _, data, _ = decrpak.split("/", maxsplit=3)
+    return data
+
+
+# Reset the keymap (sorry if you were using it lol)
+print("Resetting the keyboard")
+print("\N{CSI}2+z", end="", flush=True)  # DECPKA 5-87
+
+print("Having a little nap just in case")
+time.sleep(0.1)
+
+print("Swapping ESC and capslock")
+
+# Swap ESC and Caps
+# print('\N{DCS}"z110/30;30/110;\N{ST}')  # DECCKD 5-31
+
+keydefs: Dict[int, str] = {}
+for src in KEYMAP.values():
+    keydefs[src] = get_keydef(src)
+
+print("Got key definitons:")
+print(keydefs)
+print("Swapping everything!")
+
+for dest, src in KEYMAP.items():
+    print(
+        '\N{DCS}"y',
+        f"{dest}/{keydefs[src]}/0",
+        "\N{ST}",
+        sep="",
+        end="",
+        flush=True,
+    )  # DEKPAK 5-78
