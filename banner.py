1f5413f9697bebe0f61b1bfc1da010f6549c5cbf
Lexi Robinson <lexi@lexi.org.uk>
2022-01-29 20:43:18 +0000

    Banner program displays a banner
    ot a very good one

--- banner.py
+++ banner.py
@@ -5,23 +5,55 @@ import tty
 
 tty.setcbreak(sys.stdin.fileno())
 
+
+def write(*text) -> None:
+    sys.stdout.write("".join((str(t) for t in text)))
+
+
+CSI = "\N{CSI}"
+DCS = "\N{DCS}"
+ST = "\N{ST}"
+
+F_OFF = "0"
+F_BOLD = "1"
+F_UNDER = "4"
+F_BLINK = "5"
+F_REVERSE = "7"
+
+WIDTH = 132
+HEIGHT = 52
+WHOLE_SCREEN = f"1;1;{HEIGHT};{WIDTH}"
+
+
+def deccara(x1: int, y1: int, x2: int, y2: int, fmt: str) -> None:
+    write(CSI, y1, ";", x1, ";", y2, ";", x2, ";", fmt, "$r")  # DECCARA 5-28
+
+
+def cup(x: int, y: int) -> None:
+    write(CSI, y, ";", x, "H")  # CUP 5-8
+
+
 # Nuke the display
-print("\N{CSI}2J")  # ED - 5-180
-print("\N{CSI}95;1;1;57;131$x") # DECFRA 5-55
+def clear_display():
+    write(CSI, "2", "J")  # ED - 5-180
+    write(
+        CSI,
+        32,  # space
+        ";",
+        WHOLE_SCREEN,
+        "$x",
+    )  # DECFRA 5-55
 
-# Invert the top half
-print("\N{CSI}1;1;10;10;7$r")  # DECCARA
 
-# Move the cursor up there somewhere idk
-print("BEFORE MOVE")
-print("\N{CSI}5;50H")  # CUP
-print("AFTER MOVE")
-# print()
-# print("\N{CSI}7mwat??\N{CSI}0m")
+clear_display()
+cup(3, 10)
+write("\N{ESC}#3", "Limehouse Labs")
+cup(3, 11)
+write("\N{ESC}#4", "Limehouse Labs")
 
+deccara(1, 1, WIDTH, 20, F_REVERSE)
+
+
+# Prevent vim's run banner showing up
+cup(WIDTH, HEIGHT)
 input()
-
-print("\N{CSI}1;1;10;10;7$r")  # DECCARA
-
-input()
-
